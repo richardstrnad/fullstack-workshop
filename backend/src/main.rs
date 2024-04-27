fe66610f4075ced6a2a451b67c6252e84b23dab2
Richard Strnad <git@strnad.ch>
2024-04-27 10:32:35 +0200

    chapter 3 & 4

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::Path,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct Workshop {
+    attendees_count: i32,
+    people_like_it: bool,
+}
+
+async fn hello_world() -> impl IntoResponse {
+    "Hello World"
+}
+
+async fn hello_name(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello {name}")
+}
+
+async fn workshop_echo(Json(workshop): Json<Workshop>) -> impl IntoResponse {
+    Json(workshop)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/:name", get(hello_name))
+        .route("/your-route", post(workshop_echo));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,29 @@
+use dioxus::prelude::*;
+
+// Urls are relative to your Cargo.toml file
+const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
+
 fn main() {
-    println!("Hello, world!");
+    launch(App)
+}
+
+#[allow(non_snake_case)]
+pub fn App() -> Element {
+    let rust_basel = "Rust Basel";
+    rsx! {
+        div {
+            class: "flex w-screen h-screen bg-slate-800 text-slate-200",
+            div {
+                class: "m-auto m-4 p-8",
+                h1 {
+                    class: "font-bold font-mono text-xl",
+                    "Welcome to {rust_basel}"
+                }
+                button {
+                    class: "m-4 py-2 px-1 border border-2 border-slate-500 rounded-md bg-slate-700",
+                    "My Button!"
+                }
+            }
+        }
+    }
 }
